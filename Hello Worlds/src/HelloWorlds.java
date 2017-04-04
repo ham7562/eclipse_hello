6ff203fbf32c535b44b3057d66c2f5554d195bdf
sjkang <ham@daegu.ac.kr>
2017-04-04 09:30:05 +0900

    added 'her world'

--- Hello Worlds/src/HelloWorlds.java
+++ Hello Worlds/src/HelloWorlds.java
@@ -5,7 +5,7 @@ public class HelloWorlds {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Worlds");
 		System.out.println("Hello his World");
-
-	}
+		System.out.println("Hello her World");
+		}
 
 }
