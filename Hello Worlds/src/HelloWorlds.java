e7e6d9c238a5877229af568759176711dbd350e0
sjkang <ham@daegu.ac.kr>
2017-04-04 09:26:41 +0900

    add print line

--- Hello Worlds/src/HelloWorlds.java
+++ Hello Worlds/src/HelloWorlds.java
@@ -4,6 +4,7 @@ public class HelloWorlds {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Worlds");
+		System.out.println("Hello his World");
 
 	}
 
